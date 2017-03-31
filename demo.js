e406343af361df4d2b81e1c3c803a70c0fb4c8f9
Felix Sanz <me@felixsanz.com>
2017-03-31 18:07:07 +0200

    Updated image

---
+++ demo.js
@@ -0,0 +1,28 @@
+import compose from './compose'
+
+/**
+ * Creates a store enhancer that applies middleware to the dispatch method
+ * of the Redux store.
+ *
+ * @param {...Function} middlewares The middleware chain to be applied.
+ * @returns {Function} A store enhancer applying the middleware.
+ */
+export default function applyMiddleware(...middlewares) {
+  return (createStore) => (reducer, preloadedState, enhancer) => {
+    const store = createStore(reducer, preloadedState, enhancer)
+    let dispatch = store.dispatch
+    let chain = []
+
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (action) => dispatch(action)
+    }
+    chain = middlewares.map(middleware => middleware(middlewareAPI))
+    dispatch = compose(...chain)(store.dispatch)
+
+    return {
+      ...store,
+      dispatch
+    }
+  }
+}
